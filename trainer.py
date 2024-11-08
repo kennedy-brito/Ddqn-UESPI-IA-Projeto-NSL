d9624b931de10ec6b41ec7e34bd79ea6b651abcb
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-11-08 11:37:49 -0300

    Completed training logic, WARNING: The higher level logic is completed, but there are some important steps missing, like exploration policy and optimizing the network

---
+++ trainer.py
@@ -0,0 +1,125 @@
+import torch
+from ai.ddqn import Ddqn
+from entities import Match, Map, Agent
+from ai.experience_replay import ReplayMemory
+
+
+class Trainer:
+	
+	def __init__(
+		self,
+		model0: Ddqn,
+		model1
+		) -> None:
+		STATE_SIZE = 121*8
+
+		self.model = model0
+		self.enemy = model1
+		self.memory = ReplayMemory(1000)
+		self.target = Ddqn(STATE_SIZE)
+
+		self.target.load_state_dict(self.model.state_dict())
+
+				
+		self.rewards_per_episode = []
+		self.total_reward = 0
+		self.best_reward = -999999
+		self.step_count = 0
+	
+	def train(
+			self, 
+			epsilon_min, # the minimum value of the exploration policy
+			epsilon_init, # the initial value of the exploration rate
+			epsilon_decay, # the decay rate of the exploration rate, it will be linear
+			learning_rate, # the model learning rate
+			discount_factor_g, # the discount factor of the rewards
+			network_sync_rate, # the rate that we sync the target network with the model
+			mini_batch_size = 32, # the size of the memory sample used to train the model
+			continue_last_model = False) -> float:
+		"""
+			Receives the training parameters used and train the model
+			Return:
+				the total reward value, which should be maximized 
+		"""
+		#TODO: Add LOGS
+		#TODO: Add training logic
+		
+		# a episode is one match
+		num_actions = 9
+
+		self.optimizer = torch.optim.Adam(
+			self.model.parameters(),
+			lr=learning_rate
+		)
+		
+		#TODO: ADD EXPLORATION DECISION MAKING IN THE MODEL get_action METHOD
+		epsilon = epsilon_init
+		episode_count = 0
+
+		for episode in range(0, 1000):
+
+			self.m = Match(3, self.model, self.enemy, presentation=False, sleep_time=0.01, print_log=False)
+
+			#TODO: Could add a callback to train by turn instead of by match
+			self.m.play(self.turn_callback)
+
+			episode_reward = self.total_reward - self.previous_total_reward
+
+			self.rewards_per_episode.append(episode_reward)
+
+			if episode_reward >= self.best_reward:
+				self.best_reward = episode_reward
+				self.save_model()
+
+			if len(self.memory) > mini_batch_size:
+				self.step_count = self.m.turn - self.step_count
+
+				# sample from memory
+				mini_batch = self.memory.sample(self.mini_batch_size)
+
+				self.optimize(mini_batch, self.model, self.target)
+
+				#TODO: Implement exploration policy decay
+				# synching the networks
+				if self.step_count > network_sync_rate:
+					self.target.load_state_dict(self.model.state_dict())
+
+
+
+
+
+		return self.total_reward
+
+	#TODO: implement
+	def optimize(self, mini_batch, policy_dqn, target_dqn):
+		pass
+
+	def turn_callback(self, team: int, ID: int, previous_pos: tuple, action: int, list_agents: list[Agent]):
+
+		"""
+		What this does is get the actual view, and use that to construct the new state
+		It gets the state saved in the model, which is a previous state, and saves the previous and new total reward, they are used to calculate the episode reward
+		"""
+		if team == 0:
+			view = None
+			map = self.m.map
+			for agent in list_agents:
+				if agent.ID == ID: view = map.get_view(agent.pos)	
+			
+			state = self.model.state
+			action = action
+			new_state = [item for data in view.values() for item in data]
+			reward = self.model.get_reward(agents=list_agents)
+
+			self.memory.append(state, action, new_state, reward)
+
+			self.previous_total_reward = self.total_reward
+			self.total_reward += reward
+
+
+	#TODO: IMPLEMENT
+	def save_model(self):
+		"""
+		Save the current model in a file
+		"""
+		pass
