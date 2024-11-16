6e6a40b25a13d40578047d646e00245a362268e2
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-11-16 08:26:01 -0300

    Optimized to normalized reward

--- optimizing.py
+++ optimizing.py
@@ -80,7 +80,7 @@ for i, res in enumerate(optimizer.res):
 
 
 # Salvando no arquivo de log
-with open("log.json", "w") as log_file:
+with open("log_normalized.json", "w") as log_file:
     json.dump(optimizer.max, log_file, indent=4)  # `indent=4` para deixar legível
 
 print("Hyperparâmetros otimizados salvos no arquivo log.json!")
